dfa3f7de2e4e23c19630999728cbac123fd24ef6
Wouter Scherphof <wouter.scherphof@gmail.com>
2024-07-31 17:08:18 +0200

    first working version

--- run.sh
+++ run.sh
@@ -7,11 +7,23 @@ DOCKER_ENV=$DOCKER_ENV
 RESTART=$RESTART
 NETWORK=$NETWORK
 FILEPORT=$FILEPORT
+RUNNER=$RUNNER
 VOLUME=$VOLUME
+
+POSTFIX_DESTINATION=$POSTFIX_DESTINATION
+
+POSTFIX_PORT=$(docker4gis/port.sh "${POSTFIX_PORT:-25}")
+
+mkdir -p "$FILEPORT"
+mkdir -p "$RUNNER"
 
 docker container run --restart "$RESTART" --name "$CONTAINER" \
 	-e DOCKER_ENV="$DOCKER_ENV" \
 	--mount type=bind,source="$FILEPORT",target=/fileport \
+	--mount type=bind,source="$FILEPORT/..",target=/fileport/root \
+	--mount type=bind,source="$RUNNER",target=/runner \
 	--mount source="$VOLUME",target=/volume \
 	--network "$NETWORK" \
+	-e "$(docker4gis/noop.sh DESTINATION "$POSTFIX_DESTINATION")" \
+	-p "$POSTFIX_PORT":25 \
 	-d "$IMAGE" postfix "$@"
