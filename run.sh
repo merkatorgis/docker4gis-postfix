a8835ed327b2680914c0139c97088bf696e8d842
Azure Pipeline <pipeline@azure.com>
2024-10-22 12:53:11 +0200

    dg bump (--env-file)

--- run.sh
+++ run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 
-IMAGE=$IMAGE
-CONTAINER=$CONTAINER
-DOCKER_ENV=$DOCKER_ENV
-RESTART=$RESTART
-NETWORK=$NETWORK
-FILEPORT=$FILEPORT
-RUNNER=$RUNNER
-VOLUME=$VOLUME
-
-POSTFIX_DESTINATION=$POSTFIX_DESTINATION
+[ -z "$POSTFIX_DESTINATION" ] ||
+	echo "DESTINATION=$POSTFIX_DESTINATION" >>"$ENV_FILE"
 
 POSTFIX_PORT=$(docker4gis/port.sh "${POSTFIX_PORT:-25}")
 
@@ -18,12 +10,11 @@ mkdir -p "$FILEPORT"
 mkdir -p "$RUNNER"
 
 docker container run --restart "$RESTART" --name "$CONTAINER" \
-	-e DOCKER_ENV="$DOCKER_ENV" \
+	--env-file "$ENV_FILE" \
 	--mount type=bind,source="$FILEPORT",target=/fileport \
 	--mount type=bind,source="$FILEPORT/..",target=/fileport/root \
 	--mount type=bind,source="$RUNNER",target=/runner \
 	--mount source="$VOLUME",target=/volume \
 	--network "$NETWORK" \
-	-e "$(docker4gis/noop.sh DESTINATION "$POSTFIX_DESTINATION")" \
-	-p "$POSTFIX_PORT":25 \
-	-d "$IMAGE" postfix "$@"
+	--publish "$POSTFIX_PORT":25 \
+	--detach "$IMAGE" postfix "$@"
